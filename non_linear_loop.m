43a98a965eead0a8925f399cdd88f9d82ef84cd2
Dalton H Bermudez <34778425+DBermudez0@users.noreply.github.com>
2022-05-18 19:23:02 -0400

    Update non_linear_loop.m

--- non_linear_loop.m
+++ non_linear_loop.m
@@ -4,10 +4,10 @@ clc
 a = 1;
 
 if a == 2
-    b=1:4;
+    b=1:4; % now you can change this conditional variable to anything not just a linear vector but also a non-linear function (e.g exponential function)
     
 elseif a==1
-    b=1:2;
+    b=1:2; % now you can change this conditional variable to anything not just a linear vector but also a non-linear function (e.g exponential function)
 end
 
 for i = b
