ff0856f39d18739dc4d1baf093dcc251c4096afd
Dalton H Bermudez <34778425+DBermudez0@users.noreply.github.com>
2025-10-05 16:19:27 -0500

    Update non_linear_loop.m

--- non_linear_loop.m
+++ non_linear_loop.m
@@ -10,6 +10,6 @@ elseif a==1
     b=1:2; % now you can change this conditional variable to anything not just a linear vector but also a non-linear function (e.g exponential function)
 end
 
-for i = b
+for i = exp(b)
     i
 end
